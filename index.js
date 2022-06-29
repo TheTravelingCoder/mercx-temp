af1d07add0fb1269a36fe8dd934c67e1d8178ea3
Kevin <54876282+TheTravelingCoder@users.noreply.github.com>
2022-06-28 18:57:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function login() {
     axios.post('https://yf3367rah23onmk5ukpwfsxfre0wrlgk.lambda-url.us-west-2.on.aws/', result.value.password).then((res) => {
       if (res.data === true) {
         $('#myIFrame').removeClass('iframe-wrapper');
+        $('#login').hide();
       } else {
         Swal.fire({
           title: 'Login Failed',
