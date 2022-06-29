da76623538331da4bc47c9be502ef3431d0f3d04
Kevin <54876282+TheTravelingCoder@users.noreply.github.com>
2022-06-28 18:17:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function login() {
+  Swal.fire({
+    title: 'Login Form',
+    html: `<input type="password" id="password" class="swal2-input" placeholder="Password">`,
+    confirmButtonText: 'Sign in',
+    focusConfirm: false,
+    preConfirm: () => {
+      const password = Swal.getPopup().querySelector('#password').value
+      if (!password) {
+        Swal.showValidationMessage(`Please enter password`)
+      }
+      return { password: password }
+    }
+  }).then((result) => {
+    axios.post('https://yf3367rah23onmk5ukpwfsxfre0wrlgk.lambda-url.us-west-2.on.aws/', result.value.password).then((res) => {
+      if (res.data === true) {
+        $('#myIFrame').removeClass('iframe-wrapper');
+      } else {
+        Swal.fire({
+          title: 'Login Failed',
+          text: 'Incorrect password',
+          icon: 'error'
+        })
+      }
+    })
+  })
+}
+
+$(document).ready(function(e) {
+  $('#myIFrame').attr('src','https://zoop.io/#ewaxohobsdf12346751418213');
+  localStorage.setItem('sk+/show-sidebar-left', 'false');
+});
